6527e46099f21a7632c681c58b60c6b4b1960a59
Abhay Kadu <abhay@triplewsols.com>
2025-12-01 14:26:16 +0530

    messageerrfix

--- public/utils/SocketConnect.js
+++ public/utils/SocketConnect.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080"); // connect to server
+const socket = io("https://fitlink-vbdq.onrender.com",{
+  transports: ["websocket"]
+}); // connect to server
 
 
 export default socket
