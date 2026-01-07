4c8f3da64f81b49763eb1882d402f478db1e5617
Abhay Kadu <abhay@triplewsols.com>
2026-01-07 18:53:01 +0530

    compareimgupdate

--- src/pages/AddProgressPhoto.jsx
+++ src/pages/AddProgressPhoto.jsx
@@ -5,21 +5,47 @@ import Addimage from "../components/Addimage";
 import { useUploadImageMutation } from "../features/api/UserApi";
 import { toast } from "react-toastify";
 import { useAddProgressPhotoMutation, useGetAllProgressPhotoQuery } from "../features/api/WorkoutApi";
-function GetImagePreview({Url,setPreviewImageUrl,setGetImagePreview}){
-   return <div className="modalOverlay">
-            <div className="modalContent">
-               <div className="imagePreview">
-                   
-                        <img src={Url} alt="Selected" />
-                   
-                </div>
-                 <div className="modalActions">
-                    <button onClick={() => { setGetImagePreview(false);setPreviewImageUrl(false) }} className="cancelBtn">Cancel</button>
-                   
-                </div>
-              
-              
-              </div></div>
+function GetImagePreview({ Url, setPreviewImageUrl, setGetImagePreview }) {
+  return <div className="modalOverlay">
+    <div className="modalContent">
+      <div className="imagePreview">
+
+        <img src={Url} alt="Selected" />
+
+      </div>
+      <div className="modalActions">
+        <button onClick={() => { setGetImagePreview(false); setPreviewImageUrl(false) }} className="cancelBtn">Cancel</button>
+
+      </div>
+
+
+    </div></div>
+
+}
+function CompareImages({ UrlArray, setcompareImage }) {
+  return <div className="modalOverlay">
+    <div className="modalContent">
+      <div className="imagePreview">
+
+        <div className="compare-parent">
+          <div className="compare-image">
+            <img style={{objectFit:'cover'}} src={UrlArray[0]} />
+            <span className="label">Before</span>
+          </div>
+          <div style={{objectFit:'cover'}} className="compare-image">
+            <img src={UrlArray[1]} />
+            <span className="label">After</span>
+          </div>
+        </div>
+
+      </div>
+      <div className="modalActions">
+        <button onClick={() => { setcompareImage(false);  }} className="cancelBtn">Cancel</button>
+
+      </div>
+
+
+    </div></div>
 
 }
 export default function AddProgressPhoto() {
@@ -27,11 +53,13 @@ export default function AddProgressPhoto() {
   const [addImage, setaddImage] = useState(false)
   const [selectedfile, setselectedfile] = useState(null);
   const [Preview, setPreview] = useState(null);
-  const [PreviewImageUrl,setPreviewImageUrl]=useState(null)
-  const [getImagePreview,setGetImagePreview]=useState(false);
+  const [PreviewImageUrl, setPreviewImageUrl] = useState(null)
+  const [getImagePreview, setGetImagePreview] = useState(false);
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [compareImage, setcompareImage] = useState(false)
   let [UploadImage, { data: UploadedImage, isLoading: UploadingImage, error: errorUploading, success: UploadSuccessfull }] = useUploadImageMutation();
   let [AddUserProgressPhoto, { data: AddProgressPhotoData, isLoading: AddingProgressPhoto, error: Error, success: Success, isSuccess }] = useAddProgressPhotoMutation();
-  let { data: GetAllProgressPhoto, isLoading, isError } = useGetAllProgressPhotoQuery({});
+  let { data: GetAllProgressPhoto,refetch, isLoading, isError } = useGetAllProgressPhotoQuery({});
   async function UploadNewImage() {
     try {
       if (UploadingImage) return;
@@ -59,6 +87,7 @@ export default function AddProgressPhoto() {
   useEffect(() => {
     if (isSuccess) {
       setselectedfile(null)
+      refetch()
       toast.success("Progress Image Uploaded Succesfully!")
     }
   }, [isSuccess])
@@ -80,6 +109,33 @@ export default function AddProgressPhoto() {
     }
   }, [Success])
 
+  useEffect(() => {
+    console.log("SelectedImages", selectedImages)
+
+  }, [selectedImages])
+  function ToggleImage(ImageUrl) {
+
+    setSelectedImages((prev) => {
+      if (prev.length >= 2 && !selectedImages.includes(ImageUrl)) {
+        toast.warning("You can select at most two images")
+        return prev
+
+      }
+
+
+      if (prev.includes(ImageUrl)) {
+        // remove image
+        return prev.filter(img => img !== ImageUrl);
+      } else {
+        // add image
+        // if (selectedImages.length == 2) return toast.warning("You can select at most two images")
+
+        return [...prev, ImageUrl];
+      }
+
+    });
+  }
+
 
 
   return <div className="AddProgressPhotoContainer">
@@ -88,12 +144,12 @@ export default function AddProgressPhoto() {
       <div className="AddProgressPhotoInnerContainerTopMenu">
         <div>
           <span onClick={() => { setSelectedNav("TimeLine") }} className={SelectedNav == "TimeLine" ? "Selected" : ""}><h5>TimeLine</h5></span>
-          <span onClick={() => { return toast.warning("This Feature is under development phase"); setSelectedNav("Compare") }} className={SelectedNav == "Compare" ? "Selected" : ""}><h5>Compare</h5></span>
+          <span onClick={() => { setSelectedNav("Compare") }} className={SelectedNav == "Compare" ? "Selected" : ""}><h5>Compare</h5></span>
           <span onClick={() => { return toast.warning("This Feature is under development phase"); setSelectedNav("Notes") }} className={SelectedNav == "Notes" ? "Selected" : ""}><h5>Notes</h5></span>
 
         </div>
 
-        <span onClick={() => { setaddImage(true) }}><h5>+ Add Photo</h5></span>
+        <span onClick={SelectedNav == "Compare" ? () => { if (selectedImages.length < 2) return toast.warning("Please select two image for comparision!"); setcompareImage(true) } : () => { setaddImage(true) }}><h5>{SelectedNav == "Compare" ? "Compare" : "+ Add Photo"}</h5></span>
 
       </div>
       <div className="ProgressPhotoContainers">
@@ -103,10 +159,18 @@ export default function AddProgressPhoto() {
             <span className="ProgressPhotoCardPhotos">
               {ele?.Photos?.map((ele) => {
                 return <span onClick={() => {
+                  if (SelectedNav == "Compare") return;
                   setGetImagePreview(true)
                   setPreviewImageUrl(ele?.imageUrl)
                   console.log("image url", ele?.imageUrl)
-                }} className="ProgressPhotoCardPhoto"><img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src={ele?.imageUrl}></img></span>
+                }} className="ProgressPhotoCardPhoto">
+                  {SelectedNav == "Compare" && <input onChange={() => {
+
+                    ToggleImage(ele?.imageUrl)
+
+                  }} className="SelectImage" checked={selectedImages.includes(ele?.imageUrl)} type="Checkbox"></input>}
+
+                  <img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src={ele?.imageUrl}></img></span>
 
               })}
 
@@ -117,12 +181,13 @@ export default function AddProgressPhoto() {
 
         })}
 
+
       </div>
 
-    </div>
-    {getImagePreview&&<GetImagePreview Url={PreviewImageUrl} setPreviewImageUrl ={setPreviewImageUrl}setGetImagePreview={setGetImagePreview}  ></GetImagePreview>}
-     
-    {addImage && <Addimage UploadingImage={UploadingImage} setIsAddImageOpen={setaddImage} setSelectedFile={setselectedfile} preview={Preview} setPreview={setPreview} UploadNewImage={() => { UploadNewImage() }}></Addimage>}
 
+    </div>
+    {getImagePreview && <GetImagePreview Url={PreviewImageUrl} setPreviewImageUrl={setPreviewImageUrl} setGetImagePreview={setGetImagePreview}  ></GetImagePreview>}
+    {compareImage && <CompareImages UrlArray={selectedImages}  setcompareImage={setcompareImage}></CompareImages>}
+    {addImage && <Addimage UploadingImage={UploadingImage} setIsAddImageOpen={setaddImage} setSelectedFile={setselectedfile} preview={Preview} setPreview={setPreview} UploadNewImage={() => { UploadNewImage() }}></Addimage>}
   </div>
 }
